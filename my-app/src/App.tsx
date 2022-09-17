19633d739742510720ae8ed33c97b9ce6057bd06
Abdullah Hafeez <hafeea6@mcmaster.ca>
2022-09-17 11:29:16 -0400

    fixed typo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./compoenents/Home";
-import { Login } from "./compoenents/Login";
-import { Test } from "./compoenents/Test";
-import { Movies } from "./compoenents/Movies";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Test } from "./components/Test";
+import { Movies } from "./components/Movies";
 
 const App = () => {
   return (
